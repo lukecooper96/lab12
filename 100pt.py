58b7eba5c14ad961dedc48f0a93e86bb3db6ebde
lukecooper96 <luke.cooper96@gmail.com>
2014-10-31 13:13:27 -0500

    100 point
    plus extra because I was bored

--- 100pt.py
+++ 100pt.py
@@ -5,18 +5,15 @@
 #########################################
 
 
-# Add a button called "Right"
-# Make it so that when you press the buttons, the oval moves to the left or right
 
 from Tkinter import *
 root = Tk()
-# Create our drawpad and oval
 drawpad = Canvas(root, width=480,height=320, background='white')
 oval = drawpad.create_oval(160,160,320,320, fill="red")
 
 class MyApp:
 	def __init__(self, parent):
-	        # Make sure the drawpad is accessible from inside the function
+	
 	        global drawpad
 		self.myParent = parent  
 		self.myContainer1 = Frame(parent)
@@ -25,29 +22,67 @@ class MyApp:
 		self.button1 = Button(self.myContainer1)
 		self.button1.configure(text="Left", background= "green")
 		self.button1.grid(row=0,column=0)
-		
-	        # Add a second button!
-				
-		
-						
-		# "Bind" an action to the first button												
 		self.button1.bind("<Button-1>", self.button1Click)
-		# Create the code to bind an action to the second button
-		# Do not change "<Button-1>"
-		 
-		  
-		# This creates the drawpad - no need to change this 
+	     
+	        #button 2
+		self.button2 = Button(self.myContainer1)
+		self.button2.configure(text="Right", background= "yellow")
+		self.button2.grid(row=0,column=1)
+		self.button2.bind("<Button-1>", self.button2Click)
+		
+		#button 3
+		self.button3 = Button(self.myContainer1)
+		self.button3.configure(text="up", background= "blue")
+		self.button3.grid(row=0,column=2)
+		self.button3.bind("<Button-1>", self.button3Click)	
+		
+		#button 4
+		self.button4 = Button(self.myContainer1)
+		self.button4.configure(text="down", background= "magenta")
+		self.button4.grid(row=0,column=3)
+		self.button4.bind("<Button-1>", self.button4Click)			
+		
+																
+		self.button1.bind("<Button-1>", self.button1Click)
+	
+	
 		drawpad.pack()
 		
 
 		
 	def button1Click(self, event):   
-		# Make the oval move to the left!
-                # "global" makes sure that we can access our oval and our drawpad
 		global oval
 		global drawpad
+		drawpad.move(oval,-25,0)
+	#button 2	
+	def button2Click(self, event):
+	    global oval
+	    global drawpad
+	    drawpad.move(oval,25,0)
+	#button 3
+	def button3Click(self, event):
+	    global oval
+	    global drawpad
+	    drawpad.move(oval,0,-10)
+	#button 4
+	def button4Click(self, event):
+	    global oval
+	    global drawpad
+	    drawpad.move(oval,0,10)
 	
-	# Add the event handler for the second button to make it move right!
+	
+	
+		
+myapp = MyApp(root)
+root.mainloop()
+
+
+	
+	
+
+
+
+	
 	
 		
 myapp = MyApp(root)
