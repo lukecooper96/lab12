324de78229c3c7b4e8d1179900e2553a29577df8
lukecooper96 <luke.cooper96@gmail.com>
2014-10-31 12:44:32 -0500

    70 point

--- 70pt.py
+++ 70pt.py
@@ -27,6 +27,13 @@ class MyApp:
 		self.button3 = Button(self.myContainer1)
 		self.button3.configure(text="Test?", background="cyan")  
 		self.button3.grid(row=0,column=2)	  ### (3)
+		
+		self.button4 = Button(self.myContainer1)
+		self.button4.configure(text="Goodbye", background="orange")  
+		self.button4.grid(row=0,column=3)	  ### (4)
+		
+		
+	
 			
 	
 		
